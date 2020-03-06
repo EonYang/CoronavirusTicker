336a671bd2b9f7c03c3c1c5c80022f2cc055f42b
YangYang <yy2473@nyu.edu>
2020-03-05 22:01:04 -0500

    1st release

---
+++ CoronavirusTicker/Views/Subview.swift
@@ -0,0 +1,13 @@
+//
+//  Subview.swift
+//  CoronavirusTicker
+//
+//  Created by yangyang on 3/5/20.
+//  Copyright © 2020 yangyang. All rights reserved.
+//
+
+import Cocoa
+
+class Subview: NSObject {
+
+}
