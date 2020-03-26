f23c99be1e3f8edfe30c9656e27040fc40dfcc1a
YangYang <yy2473@nyu.edu>
2020-03-26 19:44:56 -0400

    new relsease page

---
+++ docs/precache-manifest.c8065189bb6e760a5859b2f9140cf2e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9252de1e64074a8005c42201ab147636",
+    "url": "/CoronavirusTicker/index.html"
+  },
+  {
+    "revision": "83f137606d52a957668f",
+    "url": "/CoronavirusTicker/static/css/2.61fb73e6.chunk.css"
+  },
+  {
+    "revision": "5e9c7589f799ad3d150b",
+    "url": "/CoronavirusTicker/static/css/main.dffeb374.chunk.css"
+  },
+  {
+    "revision": "83f137606d52a957668f",
+    "url": "/CoronavirusTicker/static/js/2.1dd5d5a6.chunk.js"
+  },
+  {
+    "revision": "ccabb9f17258b3dfce56c3ad108c4f70",
+    "url": "/CoronavirusTicker/static/js/2.1dd5d5a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9c7589f799ad3d150b",
+    "url": "/CoronavirusTicker/static/js/main.2b77679f.chunk.js"
+  },
+  {
+    "revision": "a28bafb35089c3da5658",
+    "url": "/CoronavirusTicker/static/js/runtime-main.78f39ea3.js"
+  }
+]);
