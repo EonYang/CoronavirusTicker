305147061623983bcdf6ddfc133f0d4dcb4d6dce
YangYang <yy2473@nyu.edu>
2020-03-26 19:57:02 -0400

    home page

---
+++ docs/precache-manifest.90efc250bdedb7dcbacf3c754997b9d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415651aa662465430ed5d744c65dee07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83f137606d52a957668f",
+    "url": "/static/css/2.61fb73e6.chunk.css"
+  },
+  {
+    "revision": "9c10e4308ef17c034735",
+    "url": "/static/css/main.dffeb374.chunk.css"
+  },
+  {
+    "revision": "83f137606d52a957668f",
+    "url": "/static/js/2.1dd5d5a6.chunk.js"
+  },
+  {
+    "revision": "ccabb9f17258b3dfce56c3ad108c4f70",
+    "url": "/static/js/2.1dd5d5a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c10e4308ef17c034735",
+    "url": "/static/js/main.80d45ffb.chunk.js"
+  },
+  {
+    "revision": "a7e0fde1229b9604647e",
+    "url": "/static/js/runtime-main.17751d76.js"
+  }
+]);
